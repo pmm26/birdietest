ec6a05c24aac8d2def012565f14cb71fe8f152ee
Pedro Roque <pmm26@protonmail.com>
2021-07-19 17:25:44 +0100

    fixed test suite

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -4,7 +4,67 @@ import { pingController } from "./controllers/ping";
 require('dotenv').config()
 
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const path = require("path");
 
+const openapi = require("express-openapi");
+
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+const port = process.env.PORT || 8000;
+
+let doc 
+try {
+  doc = yaml.load(fs.readFileSync(path.resolve(__dirname, "api/api-doc.yml"), 'utf8'));
+} catch (e) {
+  console.log(e);
+}
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use(pingController);
+
+openapi.initialize({
+  app,
+  apiDoc: doc,
+  paths: path.resolve(__dirname, "api/v1"),
+  securityHandlers: {
+    ApiKeyAuth: function (req: any, _scopes: any, _definition: any) {
+      console.log(req.headers);
+      if (req.headers["x-api-key"] != "biediekey") {
+        throw {
+          status: 401,
+          message: "You must authenticate to access birdie api.",
+        };
+      } else {
+        return Promise.resolve(true);
+      }
+    },
+  },
+});
+
+app.use(
+  "/api-documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(null, {
+    swaggerOptions: {
+      url: `http://localhost:${port}/v1/api-docs`,
+    },
+  })
+);
+
+app.use((err: any, _req: any, res: any, _next: any) => {
+  // format error
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+
+
 
 export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,53 +1,6 @@
 import app from "./application";
-const bodyParser = require("body-parser");
-const openapi = require("express-openapi");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const path = require("path");
+
 const port = process.env.PORT || 8000;
-
-const YAML = require("yamljs");
-const doc = YAML.load("src/api/api-doc.yml");
-
-app.use(cors());
-app.use(bodyParser.json());
-
-openapi.initialize({
-  app,
-  apiDoc: doc,
-  paths: path.resolve(__dirname, "api/v1"),
-  securityHandlers: {
-    ApiKeyAuth: function (req: any, _scopes: any, _definition: any) {
-      console.log(req.headers);
-      if (req.headers["x-api-key"] != "biediekey") {
-        throw {
-          status: 401,
-          message: "You must authenticate to access birdie api.",
-        };
-      } else {
-        return Promise.resolve(true);
-      }
-    },
-  },
-});
-
-app.use(
-  "/api-documentation",
-  swaggerUi.serve,
-  swaggerUi.setup(null, {
-    swaggerOptions: {
-      url: `http://localhost:${port}/v1/api-docs`,
-    },
-  })
-);
-
-app.use((err: any, _req: any, res: any, _next: any) => {
-  // format error
-  res.status(err.status || 500).json({
-    message: err.message,
-    errors: err.errors,
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
